347b8146bc3b6fc8cb6ba2e6f0005d2c07a114fb
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-14 14:51:50 -0400

    sending a POST request to the database

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,6 +2,14 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
   const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
     return;
   };
   return (
