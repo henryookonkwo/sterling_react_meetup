e665b8c4f1dd3a54ffa34df039ed0a1e7dbbf3ae
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-14 13:30:48 -0400

    adding a form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 const NewMeetupPage = () => {
   return (
     <section>
       <h1>New Meetups page</h1>
+      <NewMeetupForm />
     </section>
   );
 };
