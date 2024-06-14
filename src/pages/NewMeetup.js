dd813e303853bdfcda7705348c77553e91f041af
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-14 14:29:59 -0400

    preparing for the App for HTTP

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,13 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const addMeetupHandler = (meetupData) => {
+    return;
+  };
   return (
     <section>
       <h1>New Meetups page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 };
