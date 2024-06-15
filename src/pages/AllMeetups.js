0ae8237861f4794c0eb3d5f4e13f0c8420a7b44d
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-15 11:20:59 -0400

    handle fetching data from database. useEffect to handle side-effects and prevent infinite loop

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,36 +1,67 @@
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+// const DUMMY_DATA = [
+//   {
+//     id: "m1",
+//     title: "This is a first meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//   },
+//   {
+//     id: "m2",
+//     title: "This is a second meetup",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+//     address: "Meetupstreet 5, 12345 Meetup City",
+//     description:
+//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//   },
+// ];
 
 const AllMeetupsPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  //handle side effects and avoids us causing an infinite loop
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json"
+    )
+      .then((response) => {
+        return response.json(); //returns an object
+      })
+      .then((data) => {
+        console.log(data);
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        console.log(meetups);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
   return (
     <section>
       <h1>All Meetups page</h1>
-      {/* <ol>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
-        })}
-      </ol> */}
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 };
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -4,39 +4,41 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
   const navigate = useNavigate();
-  //   const addMeetupHandler = (meetupData) => {
-  //     fetch(
-  //       "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
-  //       {
-  //         method: "POST",
-  //         body: JSON.stringify(meetupData),
-  //         headers: { "Content-Type": "application/json" },
-  //       }
-  //     ).then(() => {
-  //       navigate("/");
-  //     });
-  //   };
-  const addMeetupHandler = async (meetupData) => {
-    try {
-      const response = await fetch(
-        "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
-        {
-          method: "POST",
-          body: JSON.stringify(meetupData),
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to send meetup data.");
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
       }
-
+    ).then(() => {
       navigate("/");
-    } catch (error) {
-      console.error("An error occurred:", error);
-      // Handle the error accordingly
-    }
+    });
   };
+
+  //   using async option
+  //   const addMeetupHandler = async (meetupData) => {
+  //     try {
+  //       const response = await fetch(
+  //         "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
+  //         {
+  //           method: "POST",
+  //           body: JSON.stringify(meetupData),
+  //           headers: { "Content-Type": "application/json" },
+  //         }
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error("Failed to send meetup data.");
+  //       }
+
+  //       navigate("/");
+  //     } catch (error) {
+  //       console.error("An error occurred:", error);
+  //       // Handle the error accordingly
+  //     }
+  //   };
 
   return (
     <section>
