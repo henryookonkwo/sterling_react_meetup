1a521f597369a56d7289f12d61575741b353cf7d
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-15 10:20:06 -0400

    navigating programmatically using useNavigate from react-router-dom

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,17 +1,43 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (meetupData) => {
-    fetch(
-      "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
-      {
-        method: "POST",
-        body: JSON.stringify(meetupData),
-        headers: { "Content-Type": "application/json" },
+  const navigate = useNavigate();
+  //   const addMeetupHandler = (meetupData) => {
+  //     fetch(
+  //       "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify(meetupData),
+  //         headers: { "Content-Type": "application/json" },
+  //       }
+  //     ).then(() => {
+  //       navigate("/");
+  //     });
+  //   };
+  const addMeetupHandler = async (meetupData) => {
+    try {
+      const response = await fetch(
+        "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json",
+        {
+          method: "POST",
+          body: JSON.stringify(meetupData),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to send meetup data.");
       }
-    );
-    return;
+
+      navigate("/");
+    } catch (error) {
+      console.error("An error occurred:", error);
+      // Handle the error accordingly
+    }
   };
+
   return (
     <section>
       <h1>New Meetups page</h1>
