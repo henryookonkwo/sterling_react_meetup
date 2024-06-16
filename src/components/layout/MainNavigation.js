5c55769a81345f9c4f434ce728640dd9b5467cef
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-16 14:36:26 -0400

    using context to add badges to nav and populate favorites page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css"; //CSS Module for scoping
-
+import FavoritesContext from "../../store/favorites-context";
 const MainNavigation = () => {
+  const favoriteCtx = useContext(FavoritesContext);
   return (
     <header className={classes.header}>
       <div className={classes.logo}>Henry's React Meetups</div>
@@ -14,7 +16,12 @@ const MainNavigation = () => {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoriteCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
