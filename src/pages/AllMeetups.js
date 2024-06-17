584aaccc8d14fcde9a783391d6a89f8a33a64bed
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-16 22:06:09 -0400

    made fatch async in AllMeetups.js

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -27,16 +27,50 @@ const AllMeetupsPage = () => {
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
   //handle side effects and avoids us causing an infinite loop
+  //Using promises
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(
+  //       "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json"
+  //     )
+  //       .then((response) => {
+  //         return response.json(); //returns an object
+  //       })
+  //       .then((data) => {
+  //         console.log(data);
+  //         const meetups = [];
+  //         for (const key in data) {
+  //           const meetup = {
+  //             id: key,
+  //             ...data[key],
+  //           };
+  //           meetups.push(meetup);
+  //         }
+  //         setIsLoading(false);
+  //         console.log(meetups);
+  //         setLoadedMeetups(meetups);
+  //       });
+  //   }, []);
+
+  //   if (isLoading) {
+  //     return (
+  //       <section>
+  //         <p>Loading...</p>
+  //       </section>
+  //     );
+  //   }
+
+  //   Using Async
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json"
-    )
-      .then((response) => {
-        return response.json(); //returns an object
-      })
-      .then((data) => {
+    const fetchMeetups = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://sterling-react-meetup-default-rtdb.firebaseio.com/meeptups.json"
+        );
+        const data = await response.json();
         console.log(data);
+
         const meetups = [];
         for (const key in data) {
           const meetup = {
@@ -45,10 +79,17 @@ const AllMeetupsPage = () => {
           };
           meetups.push(meetup);
         }
+
         setIsLoading(false);
         console.log(meetups);
         setLoadedMeetups(meetups);
-      });
+      } catch (error) {
+        console.error("Error fetching meetups:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchMeetups();
   }, []);
 
   if (isLoading) {
@@ -58,6 +99,7 @@ const AllMeetupsPage = () => {
       </section>
     );
   }
+
   return (
     <section>
       <h1>All Meetups page</h1>
